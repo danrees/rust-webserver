623b743f46149283fb0e5ea2eccad35c2ee46d28
Daniel Rees <daniel.rees@autodata.net>
2019-10-22 16:41:41 -0400

    20.1-5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,8 +17,9 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
-    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    println!("response: {}", response);
     stream.flush().unwrap();
 }
